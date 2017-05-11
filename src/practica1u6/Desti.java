bb8902aabc0d8030584314e1dbfcc3f7f908caf1
ericdote <rovx15@gmail.com>
2017-05-11 16:02:37 +0200

    Hay que certificar.

--- src/practica1u6/Desti.java
+++ src/practica1u6/Desti.java
@@ -38,44 +38,10 @@ public class Desti {
         try {
             kPrivada = loadKeyStore(alias, password).getKey(alias, password.toCharArray());
         } catch (Exception ex) {
-            Logger.getLogger(Origen.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Desti.class.getName()).log(Level.SEVERE, null, ex);
         }
         return kPrivada;
     }
     
-    /**
-     * Metode que li arriba una ruta del fitxer, un array de bytes amb el
-     * contingut d'aquest pero signat i la clau publica. Llegim el contingut del
-     * fitxer i amb Signature.verify comprovem que la firma ha sigut valida.
-     *
-     * @param fitxer
-     * @param signature
-     * @param pub
-     * @return
-     */
-    public boolean validarSignatura(String fitxer, byte[] signature, PublicKey pub) {
-        boolean isValid = false;
-        FileInputStream fis;
-        BufferedInputStream bis;
-        byte[] buffer = new byte[1024];
-        int mida;
-        try {
-            fis = new FileInputStream(fitxer);
-            bis = new BufferedInputStream(fis);
-            Signature signer = Signature.getInstance("SHA256withRSA");
-            signer.initVerify(pub);
-            while (bis.available() != 0) {
-                mida = bis.read(buffer);
-                signer.update(buffer, 0, mida);
-            }
-            bis.close();
-            isValid = signer.verify(signature);
-        } catch (NoSuchAlgorithmException | InvalidKeyException | SignatureException | FileNotFoundException ex) {
-            Logger.getLogger(Desti.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(Desti.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return isValid;
-    }
-    
+   
 }
--- src/practica1u6/Practica1u6.java
+++ src/practica1u6/Practica1u6.java
@@ -10,8 +10,6 @@ public class Practica1u6 {
     public static void main(String[] args) {
         String rutaKeyStoreDesti = "C:\\Users\\Eric\\Desktop\\DAM\\M9\\UF1\\Practica1u6\\src\\SSL\\desti.jks";
         String rutaKeyStoreOrigen = "C:\\Users\\Eric\\Desktop\\DAM\\M9\\UF1\\Practica1u6\\src\\SSL\\origen.jks";
-        Desti desti = new Desti();
-        Origen origen = new Origen();
         
     }
     
